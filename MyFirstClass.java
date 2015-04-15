9b949929c93481d40d8bdd371d74f0681b46d17e
mandy-e <Fuzz@ip0af50cab.int.uvm.edu>
2015-04-15 19:47:56 -0400

    demo class!

---
+++ MyFirstClass.java
@@ -0,0 +1,20 @@
+/* Jackie Horton
+ CS 110
+ My first JAVA program
+*/
+public class MyFirstClass 
+{
+   
+   public static void main(String [] args)
+   {
+      //Code Goes here
+     //  int temp;
+//       temp = 80;
+//       if(!(temp > 100))
+//          System.out.println("This is below max temp!");
+         int x = 1, y;
+         y = ++x;
+         System.out.println("++x = " + y);
+         
+   }
+}
